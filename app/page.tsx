7d7ba3013c717c6608c3dd132c310ec9b0af5130
CyberSpeed9967 <PM127@sctc.k12.pa.us>
2024-10-31 09:57:38 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm Preston Maust I go to  SCTC Somerset County Technical Center and im in the Computer Networking program. My sending school is Salisbury Elk-Lick Highschool.`}
       </p>
       <div className="my-8">
         <BlogPosts />
